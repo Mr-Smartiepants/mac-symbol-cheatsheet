98b6777b3496643f6381ae28de3185f339aa8d04
Mr.Smartiepants <st.stefanius@googlemail.com>
2026-03-05 10:16:48 +0100

    Initialize macOS SwiftUI project

---
+++ mac-symbol-cheatsheet/mac_symbol_cheatsheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_symbol_cheatsheetApp.swift
+//  mac-symbol-cheatsheet
+//
+//  Created by Stefan Weißgerber on 05.03.26.
+//
+
+import SwiftUI
+
+@main
+struct mac_symbol_cheatsheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
